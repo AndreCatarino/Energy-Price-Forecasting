9957da7f95404955d722894e9106a867ff5186c7
André Catarino <l56788@aln.iseg.ulisboa.pt>
2023-11-19 23:38:22 +0000

    frontend residual analysis

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -7,12 +7,18 @@ import deeplearning_build
 import dash
 import plotly.graph_objs as go
 from dash import html, dcc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 import pandas as pd
 import numpy as np
 import joblib
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import base64
+import io
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 # Prepare data and deep learning models
 
@@ -37,27 +43,78 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# App layout
-app.layout = html.Div([
-    html.H1("Energy Price Forecasting Dashboard", style={'text-align': 'center'}),
-    dcc.Dropdown(id="select_model",
-                 options=[
-                     {"label": "BiLSTM", "value": 1},
-                     {"label": "CNN-BiLSTM", "value": 2},
-                     {"label": "CNN-BiLSTM-Attention", "value": 3}],
-                 multi=False,
-                 value=1,
-                 style={'width': "40%"}
-                 ),
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-    # wrapping the graph in a loading component
-    dcc.Loading(
-        id="loading-graph",
-        children=dcc.Graph(id="model_accuracy", figure={}),
-        type="default",
-    ),
-])
+app.layout = html.Div(
+    children=[
+        html.Div(
+            [
+                html.H1("Energy Price Forecasting Dashboard", style={'text-align': 'center'}),
+                html.Hr(),
+                html.H4(id='output_container', children=[], style={'text-align': 'center'}),
+                html.Br(),
+                html.Div("Select a model to evaluate its performance on the test set:", style={'text-align': 'center'}),
+                dcc.Dropdown(
+                    id="select_model",
+                    options=[
+                        {"label": "BiLSTM", "value": 1},
+                        {"label": "CNN-BiLSTM", "value": 2},
+                        {"label": "CNN-BiLSTM-Attention", "value": 3}
+                    ],
+                    multi=False,
+                    value=1,
+                    style={"text-align": 'center'}
+                ),
+                html.Br(),
+                html.Div(
+                    id='performance_table_container',
+                    children=[],
+                    style={'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
+                ),
+                html.Br(),
+                dcc.Loading(
+                    id="loading-graph",
+                    children=dcc.Graph(id="model_accuracy", figure={}),
+                    type="default"
+                ),
+                html.Br(),
+                html.Div([
+                    dcc.Graph(id='error_distribution', figure={}),
+                ]),
+                html.Div(id='residuals-container', children=[]),
+                html.Div(id='output_plots', children=[]),
+            ],
+            style={'backgroundColor': '#f2f2f2', 'padding': '30px'}  # Light gray background color with padding
+        )
+    ]
+)
+
+def create_performance_table(model_name, mae, mse, rmse, r2):
+    performance_metrics = pd.DataFrame({
+        'Metric': [
+            #"Model chosen",
+            "Mean Absolute Error",
+            "Mean Squared Error",
+            "Root Mean Squared Error",
+            "R2 Score"
+        ],
+        'Value': [
+            #model_name,
+            round(mae, 2),
+            round(mse, 2),
+            round(rmse, 2),
+            round(r2, 2)
+        ]
+    })
+
+    performance_table = dbc.Table.from_dataframe(
+        performance_metrics,
+        striped=True,
+        bordered=True,
+        hover=True,
+        responsive=True
+    )
+
+    return performance_table
+
 
 def model_processing(df_test, model_name):
     deepL.load_model(model_name) 
@@ -77,6 +134,9 @@ def model_processing(df_test, model_name):
     scaler = joblib.load("../artifacts/scaler.pkl")
     pred = scaler.inverse_transform(y_pred)
     original_target = scaler.inverse_transform(y_batch_list)
+    
+    # Calculate the errors (residuals)
+    errors = original_target - pred
 
     mae = mean_absolute_error(original_target, pred)
     mse = mean_squared_error(original_target, pred)
@@ -109,21 +169,19 @@ def model_processing(df_test, model_name):
         x=1
     ))
 
-    # Display model performance metrics 
-    container = f"The model chosen by user was: {model_name}\n"
-    container += "Model Performance Metrics: \n"
-    container += f"Mean Absolute Error: {round(mae, 2)};\n"
-    container += f"Mean Squared Error: {round(mse, 2)};\n"
-    container += f"Root Mean Squared Error: {round(rmse, 2)}; \n"
-    container += f"R2 Score: {round(r2, 2)} \n"
+    container = f"Multivariate Sequence to Vector Modeling: Next hour price prediction using {model_name} model"
 
-    return container, fig
-    
+    return container, fig, errors, mae, mse, rmse, r2
+
 # Connect the plotly graphs with Dash Components
 @app.callback(
     [Output(component_id='output_container', component_property='children'),
      Output(component_id='model_accuracy', component_property='figure')],
-    [Input(component_id='select_model', component_property='value')]
+     Output(component_id='error_distribution', component_property='figure'),
+     Output(component_id='residuals-container', component_property='children'),
+     Output(component_id='output_plots', component_property='children'),
+     Output(component_id='performance_table_container', component_property='children'),
+    [Input(component_id='select_model', component_property='value')] 
 )
 
 
@@ -137,13 +195,48 @@ def update_graph(option_selected):
     if option_selected in models:
         model_name = models[option_selected]
         loading = True
-        container, fig = model_processing(df_test, model_name)
+        container, fig, errors, mae, mse, rmse, r2 = model_processing(df_test, model_name)
         loading = False
-        return container, fig
-    else:
-        container = "Please select a valid model"
-        fig = go.Figure()
-        return container, fig    
+
+        # Plot error distribution
+        error_fig = go.Figure()
+        error_fig.add_trace(go.Histogram(x=errors.flatten(), histnorm='probability density'))
+        error_fig.update_layout(title="Error Distribution",
+                            xaxis_title="Error",
+                            yaxis_title="Density",
+                            showlegend=False)
+
+        # Perform autocorrelation test
+        
+        # Reshape errors array if needed
+        errors = errors.flatten()
+
+        # Perform autocorrelation test
+        fig_acf, ax_acf = plt.subplots()
+        fig_pacf, ax_pacf = plt.subplots()
+
+        plot_acf(errors, ax=ax_acf)
+        plot_pacf(errors, ax=ax_pacf)
+
+        # Save the plots as base64 encoded strings in assets folder
+        buffer_acf = io.BytesIO()
+        fig_acf.savefig(buffer_acf, format='png')
+        buffer_acf.seek(0)
+        buffer_pacf = io.BytesIO()
+        fig_pacf.savefig(buffer_pacf, format='png')
+        buffer_pacf.seek(0)
+
+        # Encode images to base64 strings
+        encoded_acf = base64.b64encode(buffer_acf.read()).decode('utf-8')
+        encoded_pacf = base64.b64encode(buffer_pacf.read()).decode('utf-8')
+
+        # Create HTML images
+        acf_plot = html.Img(src=f"data:image/png;base64,{encoded_acf}")
+        pacf_plot = html.Img(src=f"data:image/png;base64,{encoded_pacf}")
+
+        performance_table = create_performance_table(model_name, mae, mse, rmse, r2)
+
+        return container, fig, error_fig, acf_plot, pacf_plot, performance_table
 
 if __name__ == "__main__":
-    app.run_server(mode='inline', host = '127.0.0.1', port='8050', debug=True)        
+    app.run_server(mode='inline', host = '127.0.0.1', port='8050', debug=True)      
